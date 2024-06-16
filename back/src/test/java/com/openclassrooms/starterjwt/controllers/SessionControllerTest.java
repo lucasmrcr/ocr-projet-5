bb19681ac7fe174c31829cc7704fbcd632e14dd3
Lucas Mercier <pro@lucasmercier.com>
2024-06-16 15:28:34 +0200

    fix(api): create SessionControllerTest and make integration tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
@@ -0,0 +1,383 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.YogaAppMvcTestFramework;
+import com.openclassrooms.starterjwt.dto.SessionDto;
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class SessionControllerTest extends YogaAppMvcTestFramework {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        sessionRepository.deleteAll();
+        teacherRepository.deleteAll();
+        getUserRepository().deleteAll();
+    }
+
+    @Test
+    public void testFindByIdInvalidInteger() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(get("/api/session/invalid")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testFindByValidId() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(get("/api/session/" + session.getId())
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Session 1"));
+    }
+
+    @Test
+    public void testFindByIdNotFound() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(get("/api/session/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(get("/api/session")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Session 1"));
+    }
+
+    @Test
+    public void testCreateInvalidSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(post("/api/session")
+                        .header("Authorization", "Bearer " + getAdminAccessToken())
+                        .contentType("application/json")
+                        .content("{}"))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testCreateValidSession() throws Exception {
+        // Given
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("Teacher");
+        teacher.setLastName("1");
+        teacher = teacherRepository.save(teacher);
+
+        SessionDto session = new SessionDto();
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 2");
+        session.setName("Session 2");
+        session.setTeacher_id(teacher.getId());
+
+        //When
+        mockMvc.perform(post("/api/session")
+                        .header("Authorization", "Bearer " + getAdminAccessToken())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(session)))
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Session 2"));
+    }
+
+    @Test
+    public void testUpdateInvalidIntegerSession() throws Exception {
+        // Given
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("Teacher");
+        teacher.setLastName("1");
+        teacher = teacherRepository.save(teacher);
+
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        sessionRepository.save(session);
+
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        sessionDto.setDescription("Description 2");
+        sessionDto.setName("Session 2");
+        sessionDto.setTeacher_id(teacher.getId());
+
+        //When
+        mockMvc.perform(put("/api/session/invalid")
+                        .header("Authorization", "Bearer " + getAdminAccessToken())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(sessionDto)))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUpdateValidSession() throws Exception {
+        // Given
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("Teacher");
+        teacher.setLastName("1");
+        teacher = teacherRepository.save(teacher);
+
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        sessionDto.setDescription("Description 2");
+        sessionDto.setName("Session 2");
+        sessionDto.setTeacher_id(teacher.getId());
+
+        //When
+        mockMvc.perform(put("/api/session/" + session.getId())
+                        .header("Authorization", "Bearer " + getAdminAccessToken())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(sessionDto)))
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Session 2"));
+    }
+
+    @Test
+    public void testDeleteNotFoundSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(delete("/api/session/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteInvalidIntegerSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(delete("/api/session/invalid")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testDeleteValidSession() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(delete("/api/session/" + session.getId())
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isOk());
+        assertEquals(0, sessionRepository.count());
+    }
+
+    @Test
+    public void testParticipateInvalidIntegerSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(post("/api/session/invalid/participate/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testParticipateSessionInvalidIntegerUser() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(post("/api/session/1/participate/invalid")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testParticipateNotFoundSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(post("/api/session/1/participate/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testParticipateSessionNotFoundUser() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(post("/api/session/"+session.getId()+"/participate/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testParticipateSessionValid() throws Exception {
+        // Given
+        User user = new User();
+        user.setFirstName("User");
+        user.setLastName("1");
+        user.setEmail("user@example.com");
+        user.setPassword("password");
+        user.setAdmin(false);
+        user = getUserRepository().save(user);
+
+
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(post("/api/session/"+session.getId()+"/participate/" + user.getId())
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isOk());
+
+        List<User> users = sessionRepository.findById(session.getId()).get().getUsers();
+        assertEquals(1, users.size());
+        assertEquals("User", users.get(0).getFirstName());
+    }
+
+    @Test
+    public void testNoLongerParticipateInvalidIntegerSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(delete("/api/session/invalid/participate/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testNoLongerParticipateSessionInvalidIntegerUser() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(delete("/api/session/"+session.getId()+"/participate/invalid")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testNoLongerParticipateNotFoundSession() throws Exception {
+        // Given
+        //When
+        mockMvc.perform(delete("/api/session/1/participate/1")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testNoLongerParticipateSessionNotParticipatingUser() throws Exception {
+        // Given
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(delete("/api/session/"+session.getId()+"/participate/100")
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testNoLongerParticipateSessionValid() throws Exception {
+        // Given
+        User user = new User();
+        user.setFirstName("User");
+        user.setLastName("1");
+        user.setEmail("user@example.com");
+        user.setPassword(getPasswordEncoder().encode("password"));
+        user.setAdmin(false);
+        user = getUserRepository().save(user);
+
+        Session session = new Session();
+        session.setName("Session 1");
+        session.setDate(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        session.setDescription("Description 1");
+        session.setUsers(new ArrayList<>());
+        session.getUsers().add(user);
+        session = sessionRepository.save(session);
+
+        //When
+        mockMvc.perform(delete("/api/session/" + session.getId() + "/participate/" + user.getId())
+                        .header("Authorization", "Bearer " + getAdminAccessToken()))
+                //Then
+                .andExpect(status().isOk());
+
+        List<User> users = sessionRepository.findById(session.getId()).get().getUsers();
+        assertEquals(0, users.size());
+    }
+
+}
