526e9d45d0fd14d47bfbf442e62e557eedae68a0
Lucas Mercier <pro@lucasmercier.com>
2024-06-16 22:11:21 +0200

    feat(front): create detail.component.spec.ts and make unit tests

--- front/src/app/features/sessions/components/detail/detail.component.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.spec.ts
@@ -1,25 +1,44 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { RouterTestingModule, } from '@angular/router/testing';
-import { expect } from '@jest/globals'; 
-import { SessionService } from '../../../../services/session.service';
-
-import { DetailComponent } from './detail.component';
-
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientModule} from '@angular/common/http';
+import {of} from 'rxjs';
+import {expect} from '@jest/globals';
+import {DetailComponent} from "./detail.component";
+import {SessionApiService} from "../../services/session-api.service";
+import {TeacherService} from "../../../../services/teacher.service";
+import {SessionService} from "../../../../services/session.service";
+import spyOn = jest.spyOn;
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>; 
-  let service: SessionService;
+  let fixture: ComponentFixture<DetailComponent>;
+
+  const sessionDetails = {
+    token: '',
+    type: '',
+    id: 1,
+    username: '',
+    firstName: '',
+    lastName: '',
+    admin: false
+  };
+
+  let sessionApiServiceMock: Partial<SessionApiService> = {
+    delete: jest.fn().mockReturnValue(of({})),
+    detail: jest.fn().mockReturnValue(of(sessionDetails)),
+    participate: jest.fn().mockReturnValue(of({})),
+    unParticipate: jest.fn().mockReturnValue(of({}))
+  };
+
+  const teacherService: Partial<TeacherService> = {
+    detail: jest.fn().mockReturnValue(of())
+  };
 
   const mockSessionService = {
-    sessionInformation: {
-      admin: true,
-      id: 1
-    }
-  }
+    sessionInformation: sessionDetails
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,18 +48,82 @@ describe('DetailComponent', () => {
         MatSnackBarModule,
         ReactiveFormsModule
       ],
-      declarations: [DetailComponent], 
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
-      service = TestBed.inject(SessionService);
+      declarations: [DetailComponent],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: sessionApiServiceMock },
+        { provide: TeacherService, useValue: teacherService }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
 
+  it('should load session details on initialization', () => {
+    // Given
+    component.sessionId = '1';
+
+    // When
+    component.ngOnInit();
+
+    // Then
+    expect(sessionApiServiceMock.detail).toHaveBeenCalled();
+    expect(sessionApiServiceMock.detail).toHaveBeenCalledWith('1');
+
+    expect(component.session).toEqual(sessionDetails);
+  });
+
+  it('should navigate to previous page when back is clicked', () => {
+    // Given
+    const backSpy = spyOn(window.history, 'back');
+
+    // When
+    component.back();
+
+    // Then
+    expect(backSpy).toHaveBeenCalled();
+  })
+
+  it('should call delete function on delete', () => {
+    // Given
+    component.sessionId = '1';
+    const sessionApiServiceDeleteSpy = spyOn(sessionApiServiceMock, 'delete');
+    const matSnackBarOpenSpy = spyOn(TestBed.inject(MatSnackBar), 'open');
+
+    // When
+    component.delete();
+
+    // Then
+    expect(sessionApiServiceDeleteSpy).toHaveBeenCalled();
+    expect(matSnackBarOpenSpy).toHaveBeenCalledWith('Session deleted !', 'Close', { duration: 3000 });
+  });
+
+  it('should call participate function on participate', () => {
+    // Given
+    const sessionApiServiceParticipateSpy = spyOn(sessionApiServiceMock, 'participate');
+
+    // When
+    component.participate();
+
+    // Then
+    expect(sessionApiServiceParticipateSpy).toHaveBeenCalled();
+  });
+
+  it('should call unParticipate function on unParticipate', () => {
+    // Given
+    const sessionApiServiceUnParticipateSpy = spyOn(sessionApiServiceMock, 'unParticipate');
+
+    // When
+    component.unParticipate();
+
+    // Then
+    expect(sessionApiServiceUnParticipateSpy).toHaveBeenCalled();
+  });
+
+
+});
