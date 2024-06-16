7a1f73e3bf0f593bf75b9b9b359ecf992dc0ab42
Lucas Mercier <pro@lucasmercier.com>
2024-06-16 23:49:24 +0200

    feat(front): create login-user.cy.ts and make e2e test

---
+++ front/cypress/e2e/login-user.cy.ts
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('Login', () => {
+  it('should login successfully', () => {
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {statusCode: 200});
+
+    cy.get('input[formControlName=email]').type('example@example.com');
+    cy.get('input[formControlName=password]').type(`password{enter}{enter}`);
+    cy.url().should('include', '/sessions');
+  });
+
+  it('should logout successfully', () => {
+    cy.url().should('include', '/sessions');
+    cy.get('.mat-toolbar > .ng-star-inserted > :nth-child(3)').click();
+    cy.url().should('not.contain', '/sessions');
+  });
+
+
+  it('should login button be disabled while email field is empty', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=password]').type(`password{enter}{enter}`);
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+
+  it('should login button be disabled while password field is empty', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').type(`example@example.com{enter}{enter}`);
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+  it('should not login with bad credentials', () => {
+    cy.visit('/login');
+
+    cy.get('input[formControlName=email]').type('example@example.com');
+    cy.get('input[formControlName=password]').type(`password{enter}{enter}`);
+    cy.get('.error').should('be.visible');
+  });
+
+});
