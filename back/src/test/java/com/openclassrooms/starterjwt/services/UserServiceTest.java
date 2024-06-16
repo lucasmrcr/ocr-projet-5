ae35aca46d631f5b15cc8a3479e05f8d63706bda
Lucas Mercier <pro@lucasmercier.com>
2024-06-16 15:37:36 +0200

    feat(api): add user model checks

--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,9 +52,16 @@ public class UserServiceTest {
     @Test
     public void findUserWhenExists() {
         // Given
+        LocalDateTime now = LocalDateTime.now();
         User userToCreate = new User();
         userToCreate.setId(1L);
         userToCreate.setEmail("example@example.com");
+        userToCreate.setAdmin(false);
+        userToCreate.setPassword("password");
+        userToCreate.setLastName("Doe");
+        userToCreate.setFirstName("John");
+        userToCreate.setCreatedAt(now);
+        userToCreate.setUpdatedAt(now);
         when(userRepository.findById(1L)).thenReturn(Optional.of(userToCreate));
 
         // When
@@ -62,7 +70,14 @@ public class UserServiceTest {
         // Then
         verify(userRepository).findById(1L);
         assertNotNull(user);
+        assertEquals(1L, user.getId());
         assertEquals("example@example.com", user.getEmail());
+        assertFalse(user.isAdmin());
+        assertEquals("password", user.getPassword());
+        assertEquals("Doe", user.getLastName());
+        assertEquals("John", user.getFirstName());
+        assertEquals(now, user.getCreatedAt());
+        assertEquals(now, user.getUpdatedAt());
     }
 
 }
