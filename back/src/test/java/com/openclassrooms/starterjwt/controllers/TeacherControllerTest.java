993a7bd3816daabe71f1bbc5c0ca6be8d66ed8e9
Lucas Mercier <pro@lucasmercier.com>
2024-06-16 14:14:51 +0200

    fix(api): id never found because of incremental id never which is never reset between tests

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerTest.java
@@ -3,8 +3,6 @@ package com.openclassrooms.starterjwt.controllers;
 import com.openclassrooms.starterjwt.AsAdminMvcTestFramework;
 import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.repository.TeacherRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,21 +15,6 @@ public class TeacherControllerTest extends AsAdminMvcTestFramework {
 
     @Autowired
     private TeacherRepository teacherRepository;
-
-    @BeforeEach
-    public void setUp() {
-        super.setUp();
-        Teacher teacher = new Teacher();
-        teacher.setFirstName("John");
-        teacher.setLastName("Doe");
-        teacherRepository.save(teacher);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        super.tearDown();
-        teacherRepository.deleteAll();
-    }
 
     @Test
     public void testFindByIdInvalidInteger() throws Exception {
@@ -56,16 +39,27 @@ public class TeacherControllerTest extends AsAdminMvcTestFramework {
     @Test
     public void testFindByIdValid() throws Exception {
         // Given
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("John");
+        teacher.setLastName("Doe");
+        teacherRepository.save(teacher);
+
         // When
-        mockMvc.perform(get("/api/teacher/1")
+        mockMvc.perform(get("/api/teacher/" + teacher.getId())
                         .header("Authorization", "Bearer " + getAdminAccessToken()))
                 // Then
                 .andExpect(status().isOk());
+        teacherRepository.deleteAll();
     }
 
     @Test
     public void testFindAllTeachers() throws Exception {
         // Given
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("John");
+        teacher.setLastName("Doe");
+        teacherRepository.save(teacher);
+
         // When
         mockMvc.perform(get("/api/teacher")
                         .header("Authorization", "Bearer " + getAdminAccessToken()))
@@ -74,5 +68,6 @@ public class TeacherControllerTest extends AsAdminMvcTestFramework {
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].firstName").value("John"))
                 .andExpect(jsonPath("$[0].lastName").value("Doe"));
+        teacherRepository.deleteAll();
     }
 }
